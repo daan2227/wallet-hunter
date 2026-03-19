67730ca993069d8d714aa6186b36a97bcc1e9b06
daan2227 <daanrodmo4@gmail.com>
2026-03-18 23:01:58 -0600

    fix: add missing closing brace to WalletManager

--- app/src/main/java/com/hunter/btc/WalletManager.kt
+++ app/src/main/java/com/hunter/btc/WalletManager.kt
@@ -262,3 +262,4 @@ object WalletManager {
         prefs.edit().remove(PREF_SEED).remove("seed_iv").apply()
         try { KeyStore.getInstance("AndroidKeyStore").also{it.load(null)}.deleteEntry(KEY_ALIAS) } catch(e: Exception) {}
     }
+}
